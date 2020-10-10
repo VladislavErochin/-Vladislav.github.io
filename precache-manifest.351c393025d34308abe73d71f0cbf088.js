e80c1b80b5e32251ec19a8508dabc3af6a56ea63
unknown <gmail@cluxinierdsny.com>
2020-10-10 13:51:19 +0300

    Updates

---
+++ precache-manifest.351c393025d34308abe73d71f0cbf088.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d782d200467e707b7d83f615bb0fea12",
+    "url": "/VladislavErochin/create-js-react/index.html"
+  },
+  {
+    "revision": "e0a3bc2f5373bb49e112",
+    "url": "/VladislavErochin/create-js-react/static/js/2.c4aef885.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VladislavErochin/create-js-react/static/js/2.c4aef885.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba264510e800c9f06c18",
+    "url": "/VladislavErochin/create-js-react/static/js/main.52814f34.chunk.js"
+  },
+  {
+    "revision": "838317432c8fcc6a582a",
+    "url": "/VladislavErochin/create-js-react/static/js/runtime-main.a7f4bc2e.js"
+  }
+]);
