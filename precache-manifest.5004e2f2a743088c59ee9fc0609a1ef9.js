18a4671b03e6a3363e1ed4062a39ff0b96a0f1e5
unknown <gmail@cluxinierdsny.com>
2020-10-10 14:12:08 +0300

    Updates

---
+++ precache-manifest.5004e2f2a743088c59ee9fc0609a1ef9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0edfd3e6f26c4b73443be02931489f76",
+    "url": "/create-react-app/index.html"
+  },
+  {
+    "revision": "e0a3bc2f5373bb49e112",
+    "url": "/create-react-app/static/js/2.c4aef885.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/create-react-app/static/js/2.c4aef885.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba264510e800c9f06c18",
+    "url": "/create-react-app/static/js/main.52814f34.chunk.js"
+  },
+  {
+    "revision": "cf5351ae4962a162703f",
+    "url": "/create-react-app/static/js/runtime-main.24929951.js"
+  }
+]);
